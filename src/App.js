3965b56f44c688bc73837fc29b417f4e722b7090
Kevin Orta <kevino6532@hstat.org>
2024-08-21 21:36:13 -0400

    added Schools Info

--- src/App.js
+++ src/App.js
@@ -2,22 +2,38 @@
 
 import React, { useEffect, useState } from "react";
 import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from "@vis.gl/react-google-maps";
+import { renderSchoolInfo } from "./components/SchoolInfo";
+import { render } from "@testing-library/react";
 
 //location of API Key
-const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+const GOOGLE_MAPS_API_KEY = "AIzaSyBRVx-TdsWWS5bnRaKBL2awiuYTWL1Alao";
 
-function SchoolInfo() {
+function SchoolInfo({ schoolName, centerName, centerInfo, email, phone, appointmentInfo, link }) {
   return (
     <div className="InfoSections">
-      <h1>Hostos Community College</h1>
-      <h2>Hostos One Stop Center</h2>
-      <h2>Savoy Building (1st floor intake) D-101</h2>
-      <h2>mcruz@hostos.cuny.edu</h2>
-      <h2>718-518-4141</h2>
-      <h2>Appointment only. Contact Above</h2>
+      <h1>{schoolName}</h1>
+      <h2>{centerName}</h2>
+      <h2>{centerInfo}</h2>
+      <h2><a href={`mailto:${link}`}>{email}</a></h2>
+      <h2>{phone}</h2>
+      <h2>{appointmentInfo}. Contact Above</h2>
     </div>
   );
 }
+
+const schoolData = [
+  {
+    schoolName: "Hostos Community College",
+    centerName: "HCC Food Pantry",
+    centerInfo: "One Stop Center, Savoy Building, Fl 1 D-101, Bronx, NY 10451",
+    email: "mcruz@hostos.cuny.edu",
+    phone: "718-518-4141",
+    appointmentInfo: "Appointment only",
+    link: "mcruz@hostos.cuny.edu"
+  }
+];
+
+
 
 function MapComponent() {
   const [position, setPosition] = useState(null);
@@ -42,7 +58,7 @@ function MapComponent() {
     <APIProvider apiKey={GOOGLE_MAPS_API_KEY}>
       <div style={{ height: "100vh", width: "100%" }}>
         {position && (
-          <Map zoom={14} center={position}>
+          <Map zoom={14} center={position} mapId={"d84e98c48a8a04c"}>
             <AdvancedMarker position={position} onClick={() => setOpen(true)}>
               <Pin
                 background={"grey"}
@@ -68,7 +84,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <MapComponent />
-        <SchoolInfo />
+        <div className="School-info-section">
+          {renderSchoolInfo()}
+        </div>
       </header>
     </div>
   );
